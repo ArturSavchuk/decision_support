e2da8a988976bc3db9f0fbf453dbeac6c4e178b0
Arturich <artursavchuk7@gmail.com>
2021-03-21 19:57:39 +0200

    implementation of comparison of binary relations

---
+++ Lab3/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CriteriaRelation criteriaRelation = ReadCriteriaRelation();
+            Console.WriteLine("Сигма вектори:");
+            PrintCriteriaRelationVectors(criteriaRelation, () => criteriaRelation.SigmaVectors);
+            Console.WriteLine("Вiдношення Парето:");
+            PrintRelation(criteriaRelation.ParetoRelation, () => criteriaRelation.ParetoRelation.Connections);
+            PrintRelation(criteriaRelation.ParetoRelation, () => criteriaRelation.ParetoRelation.Characteristic);
+            Console.WriteLine("Мажоритарне вiдношення:");
+            PrintRelation(criteriaRelation.MajorityRelation, () => criteriaRelation.MajorityRelation.Connections);
+            PrintRelation(criteriaRelation.MajorityRelation, () => criteriaRelation.MajorityRelation.Characteristic);
+            Console.WriteLine("Лексикографiчне вiдношення:");
+            PrintRelation(criteriaRelation.LexicographicRelation, () => criteriaRelation.LexicographicRelation.Connections);
+            PrintRelation(criteriaRelation.LexicographicRelation, () => criteriaRelation.LexicographicRelation.Characteristic);
+            Console.WriteLine("Вiдношення Березовського:");
+            PrintRelation(criteriaRelation.BerezovskyRelation, () => criteriaRelation.BerezovskyRelation.Connections);
+            PrintRelation(criteriaRelation.BerezovskyRelation, () => criteriaRelation.BerezovskyRelation.Characteristic);
+            Console.WriteLine("Вiдношення Подиновського:");
+            PrintRelation(criteriaRelation.PodinovskyRelation, () => criteriaRelation.PodinovskyRelation.Connections);
+            PrintRelation(criteriaRelation.PodinovskyRelation, () => criteriaRelation.PodinovskyRelation.Characteristic);
+
+            WriteResults(criteriaRelation);
+        }
+
+        public static CriteriaRelation ReadCriteriaRelation()
+        {
+            string directoryPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string fileName = $"{directoryPath}\\Lab3\\relations_var52.txt";
+            string[] allFileLines = File.ReadAllLines(fileName);
+
+            int[][] relation = new int[20][];
+            for (int i = 0; i < 20; i++)
+            {
+                relation[i] = allFileLines[i].Split(' ')
+                    .Where(s => !string.IsNullOrWhiteSpace(s))
+                    .Select(s => int.Parse(s))
+                    .ToArray();
+            }
+
+            HashSet<int> criteriasImportance =
+                new HashSet<int> { 10,11,5,8,1,9,4,7,2,12,6,3 }.Select(c => c - 1).ToHashSet();
+            List<HashSet<int>> criteriasImportancesClasses =
+                new List<HashSet<int>>
+                {
+                    new HashSet<int> { 2, 4, 7 }.Select(c => c - 1).ToHashSet(),
+                    new HashSet<int> { 6, 9}.Select(c => c - 1).ToHashSet(),
+                    new HashSet<int> { 1, 3, 5, 8, 10, 11, 12}.Select(c => c - 1).ToHashSet()
+                };
+
+            return new CriteriaRelation(relation, criteriasImportance, criteriasImportancesClasses);
+        }
+
+        public static void WriteResults(CriteriaRelation criteriaRelation)
+        {
+            string directoryPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string fileName = $"{directoryPath}\\Lab3\\Var52-ArturSavchuk.txt";
+
+            File.AppendAllLines(fileName,
+                new List<string>
+                {
+                    "1",
+                    criteriaRelation.ParetoRelation.ToString(),
+                    "2",
+                    criteriaRelation.MajorityRelation.ToString(),
+                    "3",
+                    criteriaRelation.LexicographicRelation.ToString(),
+                    "4",
+                    criteriaRelation.BerezovskyRelation.ToString(),
+                    "5",
+                    criteriaRelation.PodinovskyRelation.ToString(),
+                });
+        }
+
+        public static void PrintRelation<T>(Relation relation, Func<T[][]> printingSelector)
+        {
+            T[][] toPrint = printingSelector();
+            Console.WriteLine();
+            Console.WriteLine($"{string.Join(' '.ToString(), relation)}");
+            Console.WriteLine();
+            for (int i = 0; i < relation.Dimension; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"{i}{string.Concat(Enumerable.Repeat(' ', 3 - i.ToString().Length))}");
+                Console.ForegroundColor = ConsoleColor.White;
+                for (int j = 0; j < relation.Dimension; j++)
+                {
+                    Console.Write($"{toPrint[i][j]}{string.Concat(Enumerable.Repeat(' ', (j + 1).ToString().Length))}");
+                }
+                Console.WriteLine();
+                
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+        }
+
+        public static void PrintCriteriaRelationVectors(CriteriaRelation criteriaRelation, Func<List<int>[][]> printingSelector)
+        {
+            List<int>[][] toPrint = printingSelector();
+
+            for (int i = 0; i < criteriaRelation.AlternativesCount; i++)
+            {
+                for (int j = 0; j < criteriaRelation.AlternativesCount; j++)
+                {
+                    Console.WriteLine($"[{i}][{j}]: {string.Join(' '.ToString(), toPrint[i][j])}");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
